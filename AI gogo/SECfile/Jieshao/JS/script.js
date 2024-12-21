04e081edbd689fc136c8b9a8e9bd8a7261578313
Azhen9823 <1754477388@qq.com>
2024-12-21 14:59:06 +0800

    Delete AI gogo directory

--- AI gogo/SECfile/Jieshao/JS/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function () {
-  const nav = document.querySelector("nav");
-  nav.style.opacity = 1 - window.pageYOffset / 200;
-  if (nav.style.opacity < 0.2) {
-    nav.style.opacity = 0.2;
-  }
-});
