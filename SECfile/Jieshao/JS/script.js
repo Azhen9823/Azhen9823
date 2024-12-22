0f31106c1d2ffcbaa2ce2daf8f9a5f57c487fd04
Azhen9823 <1754477388@qq.com>
2024-12-22 17:23:07 +0800

    Update script.js

--- SECfile/Jieshao/JS/script.js
+++ SECfile/Jieshao/JS/script.js
@@ -5,3 +5,27 @@ window.addEventListener("scroll", function () {
     nav.style.opacity = 0.2;
   }
 });
+
+/*以下部分用来控制更多按钮（12.22）*/
+document.addEventListener("DOMContentLoaded", function () {
+  const loadMoreBtn = document.getElementById("load-more-btn");
+  const hiddenArticles = document.querySelectorAll(".hidden-article");
+  window.addEventListener("scroll", function () {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    const loadMoreBtnOffsetTop =
+      loadMoreBtn.getBoundingClientRect().top + scrollTop;
+    if (loadMoreBtnOffsetTop - windowHeight < 0) {
+      hiddenArticles.forEach(function (article) {
+        article.style.display = "block";
+      });
+      loadMoreBtn.style.display = "none";
+    }
+  });
+  loadMoreBtn.addEventListener("click", function () {
+    hiddenArticles.forEach(function (article) {
+      article.style.display = "block";
+    });
+    loadMoreBtn.style.display = "none";
+  });
+});
